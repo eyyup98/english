140960e9a1bbd277728fd670567315d747fca752
eyyup98 <iyov14.01.98@gmail.com>
2023-03-03 18:13:11 +0300

    j: home navbar citiesSearch

--- app/Modules/SearchCities/src/Controllers/SearchCitiesController.php
+++ app/Modules/SearchCities/src/Controllers/SearchCitiesController.php
@@ -5,15 +5,16 @@ namespace App\Modules\SearchCities\src\Controllers;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class SearchCitiesController extends Controller
 {
-    public function searchCity(Request $request)
+    public function searchCity(Request $request): JsonResponse
     {
         $cities = DB::select("SELECT name FROM cities WHERE name LIKE '$request->city%' ORDER BY name");
-        return response()->json($cities);
+        return response()->json(!empty($cities) ? $cities : [0 => ['name' => 'Город не найден']]);
     }
 
 }
